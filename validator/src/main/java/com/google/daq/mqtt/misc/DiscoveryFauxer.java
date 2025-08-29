6aba6c52984902714e6ec94e7bc45cf16573a363
Trevor Pering <peringknife@google.com>
2025-08-29 11:17:49 -0700

    Initial faux discovery shell

---
+++ validator/src/main/java/com/google/daq/mqtt/misc/DiscoveryFauxer.java
@@ -0,0 +1,14 @@
+package com.google.daq.mqtt.misc;
+
+/**
+ * Simple converer for some foreign setup files into synthetic discovery events.
+ */
+public class DiscoveryFauxer {
+
+  /**
+   * Let's go.
+   */
+  public static void main(String[] args) {
+    System.err.println("Hello.");
+  }
+}
